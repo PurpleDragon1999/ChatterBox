2d9cb48dc4742e33fe5249dcd9273e0f734b2d63
Shubham Sharma <ss320163@gmail.com>
2020-06-08 21:33:52 +0530

    Build is uploaded

---
+++ web/build/precache-manifest.dddc3636925efed86b49ac0949d6003e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5939fc26bab8ae691fa0a7a609b40f70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f79acde893887983d114",
+    "url": "/static/css/main.63f42ad3.chunk.css"
+  },
+  {
+    "revision": "cb118be528647eebcc82",
+    "url": "/static/js/2.4b112749.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.4b112749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79acde893887983d114",
+    "url": "/static/js/main.7e9d5c01.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  }
+]);
